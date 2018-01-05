8d04b57f112ef0654aef014ce751b030d69d77a8
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-01-05 15:25:41 -0800

    create server exercise

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// very important to do this app.get BELOW line 2, where the app was initially created
+app.get('/', function(req, res) {
+  console.log('hello this is the server')
+  res.send('hello this is the client')
+})
+
 const listener = () => console.log(`Application listening on port ${port}!`)
 app.listen(port, listener)
